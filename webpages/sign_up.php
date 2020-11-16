5a0a7d0ad90d57643738e3f8e8e75992166420a8
Matthew Rupchan <matthew.rupchan@gmail.com>
2020-11-15 21:51:05 -0600

    Fix_Server
    woops

--- webpages/sign_up.php
+++ webpages/sign_up.php
@@ -1,8 +1,7 @@
 <!DOCTYPE HTML>
 <?php
 	//database variables
-	//$dbserver = "34.121.103.176:3306";
-	$dbserver = "localhost";
+	$dbserver = "34.121.103.176:3306";
 	$dbusername = "testuser1587";
 	$dbpassword = "woai1587";
 	$dbname = "catsdatabase";
