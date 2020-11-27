0bf484671133b764c914d6a6d41400d7b35fe55e
Matthew Rupchan <matthew.rupchan@gmail.com>
2020-11-27 12:37:47 -0600

    Do_Interaction_Backend
    Also updated some glitches on the marketplace page. But now the interaction page will fill in, update timestamps and money when interactions are done. The interaction cooldown is 15 minutes. The selling form executes and changes with ease.  The user is redirected away from the page if the cat isn't theirs, the cat is invalid, or if the user isn't logged in.

--- webpages/interaction.php
+++ webpages/interaction.php
@@ -17,6 +17,7 @@
 		header("Location: index.php");
 	} 
 	
+	//check if this is a page for a real cat and if it is yours
 	if (!isset($_GET["cat_id"])) {
 		header("Location: suite.php");
 	} else {
@@ -25,17 +26,95 @@
 			die("Connection failed: " . $database->connect_error);
 		}
 		$cat_id = $_GET["cat_id"];
-		$query = "SELECT user_id FROM cat_table WHERE cat_id = '$cat_id';"; 
+		$query = "SELECT * FROM cat_table WHERE cat_id = '$cat_id';"; 
 		
 		$results = $database->query($query);
 		$database->close();
 		
-		$results = $results->fetch_assoc();
-		$user_id = $results["user_id"];
+		$cat_info = $results->fetch_assoc();
+		$user_id = $cat_info["user_id"];
 		
 		if ($user_id != $_SESSION["user"]) { //not your cat goodbye
 			header("Location: suite.php");
 		}
+	}
+	
+	//money control variables
+	$standardGift = 5;
+	$boostedGift = 10;
+	$update_needed = false;
+	if (isset($_POST["feed"]) && $_POST["feed"] == 1) {
+		//personality traits that boost money
+		$boost1 = "Needy";
+		$boost2 = "Greedy";
+		//timer to update
+		$timer_name = "feed_timer";
+		$update_needed = true;
+	} elseif(isset($_POST["pet"]) && $_POST["pet"] == 1) {
+		//personality traits that boost money
+		$boost1 = "Dozy";
+		$boost2 = "Snuggly";
+		//timer to update
+		$timer_name = "pet_timer";
+		$update_needed = true;
+	} elseif(isset($_POST["play"]) && $_POST["play"] == 1) {
+		//personality traits that boost money
+		$boost1 = "Playful";
+		$boost2 = "Feisty";
+		//timer to update
+		$timer_name = "play_timer";
+		$update_needed = true;
+	} elseif(isset($_POST["groom"]) && $_POST["groom"] == 1) {
+		//personality traits that boost money
+		$boost1 = "Friendly";
+		$boost2 = "Easy-going";
+		//timer to update
+		$timer_name = "groom_timer";
+		$update_needed = true;
+	}
+	
+	if ($update_needed) {
+		//increase money
+		if ($cat_info["personality"] == $boost1 || $cat_info["personality"] == $boost2) {
+			$new_money = $_SESSION["money"] + $boostedGift;
+		} else {
+			$new_money = $_SESSION["money"] + $standardGift;
+		}
+		updateMoney($new_money);
+		//update timestamp
+		setTimestamp($timer_name, $cat_id);
+		//refresh timestamp variables
+		$database = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+		if ($database->connect_error) {
+			die("Connection failed: " . $database->connect_error);
+		}
+		$cat_id = $_GET["cat_id"];
+		$query = "SELECT * FROM cat_table WHERE cat_id = '$cat_id';"; 
+		$results = $database->query($query);
+		$database->close();
+		$cat_info = $results->fetch_assoc();
+	}
+	
+	//Updating cat's for sale status if the forms are submitted
+	if(isset($_POST["cat_price"]) && $_POST["cat_price"] > 0) {
+		$database = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+		if ($database->connect_error) {
+			die("Connection failed: " . $database->connect_error);
+		}
+		$cost = $_POST["cat_price"];
+		$query = "UPDATE cat_table SET cost = '$cost' WHERE cat_id = '$cat_id';";
+		$database->query($query);
+		$database->close();
+		$cat_info["cost"] = $cost;
+	} elseif (isset($_POST["remove_from_marketplace"]) && $_POST["remove_from_marketplace"] == 1) {
+		$database = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+		if ($database->connect_error) {
+			die("Connection failed: " . $database->connect_error);
+		}
+		$query = "UPDATE cat_table SET cost = NULL WHERE cat_id = '$cat_id';";
+		$database->query($query);
+		$database->close();
+		$cat_info["cost"] = null;
 	}
 ?>
 
@@ -44,7 +123,6 @@
 		<title>Suite Cats</title>
 		<link rel="stylesheet" type="text/css" href="../css/style.css">
 		<link rel="stylesheet" type="text/css" href="../css/interaction.css">
-		<script type="text/javascript" src="../javascript/insertjavascriptfilehere.js"></script>
 	</head>
 	
 	<body>
@@ -52,12 +130,14 @@
 		<header>
 			<div id="website_header"> 
 				<a href="index.php"><h3 id="website_title">Suite Cats</h3></a>
-				<img id="mascot" src="../cat_images/placeholder.png" alt="Mascot">
+				<img id="mascot" src="../cat_images/icons/Mascot.png" alt="Mascot">
 				<div id="user_info_box">
-					<div id="username" class="user_box_element">Username<!--PHP INPUT--></div>
-					<div id="money" class="user_box_element">$420<!--PHP INPUT--></div>
-					<button id="log_out" class="user_box_element">Log Out</button>
-				</div>
+					<div id="username" class="user_box_element"><?=$_SESSION["user_name"]?></div>
+					<div id="money" class="user_box_element">$<?=$_SESSION["money"]?></div>
+					<form action="index.php" method="post" enctype="multipart/form-data" id="logout">
+						<input type="hidden" name="logout" value="1"></input>
+						<button id="log_out" type="submit" class="user_box_element">Log Out</button>
+					</form>
 			</div>
 			
 			<menu>
@@ -74,58 +154,158 @@
 			cat details
 			-->
 			<div id="left">
-				<h1 id="CatName"><!--PHP SPOT-->Name</h1>
-				<div class="details">Tail Type:</div><div class="info" id="tail"><!--PHP SPOT-->Excellent</div>
+				<h1 id="CatName"><?=$cat_info["cat_name"]?></h1>
+				<div class="details">Tail Type:</div><div class="info" id="tail"><?=$cat_info["tail_type"]?></div>
 				<br>
-				<div class="details">Hair Length:</div><div class="info" id="hair"><!--PHP SPOT-->Nice</div>
+				<div class="details">Hair Length:</div><div class="info" id="hair"><?=describeHair($cat_info["hair_length"])?></div>
 				<br>
-				<div class="details">Colour:</div><div class="info" id="colour"><!--PHP SPOT-->Wonderful</div>
+				<div class="details">Colour:</div><div class="info" id="colour"><?=$cat_info["body_colour"]?></div>
 				<br>
-				<div class="details">Eye Colour:</div><div class="info" id="eye"><!--PHP SPOT-->Perfect</div>
+				<div class="details">Eye Colour:</div><div class="info" id="eye"><?=$cat_info["eye_colour"]?></div>
 				<br>
-				<div class="details">Personality:</div><div class="info" id="personality"><!--PHP SPOT-->Exciting</div>
+				<div class="details">Personality:</div><div class="info" id="personality"><?=$cat_info["personality"]?></div>
 				<br>
-				<div class="details">Gender:</div><div class="info" id="gender"><!--PHP SPOT-->Needs Work</div>
+				<div class="details">Gender:</div><div class="info" id="gender"><?=$cat_info["gender"]?></div>
 			</div>
 			
 			<!-- 
 			cat avatar and selling
 			-->
 			<div id="middle">
-				<img id="avatar" src="../cat_images/placeholder.png" alt="Cat Image">
+				<img id="avatar" src="../<?=$cat_info["Img_URL"]?>" alt="Cat Image">
 				<br>
-				<form id="sell">
+				<?php 
+					if($cat_info["cost"] == null) { //The cat is not for sale
+				?>
+				<form id="sell" action="interaction.php?cat_id=<?=$cat_id?>" method="post" enctype="multipart/form-data">
 					<input id="sell_cat" type="submit" value="Sell"></button>
-					<input id="cat_price" type="number" value="0" min="0"></input>
+					<input id="cat_price" name="cat_price" type="number" value="1" min="1"></input>
 					<div id="price_label">Price:</div>
 				</form>
+				<?php 
+					} else { //The cat is for sale
+				?>
+				<form id="sell" action="interaction.php?cat_id=<?=$cat_id?>" method="post" enctype="multipart/form-data">
+					<div id="for_sale_label">For sale in the <a href="Marketplace.php">Marketplace</a></div>
+					<input type="hidden" name="remove_from_marketplace" value=1></input>
+					<input id="stop_sell_cat" type="submit" value="Stop Selling Cat"></button>
+				</form>
+					
+				<?php 
+					}
+				?>
 			</div>
 			
 			<!-- 
 			cat interactions
 			-->
+			<?PHP
+				$cooldown_time = 900; //900 seconds is 15 minutes
+				$feed_time = getReadyTime($cat_info["feed_timer"], $cooldown_time);
+				$pet_time = getReadyTime($cat_info["pet_timer"], $cooldown_time);
+				$play_time = getReadyTime($cat_info["play_timer"], $cooldown_time);
+				$groom_time = getReadyTime($cat_info["groom_timer"], $cooldown_time);
+			?>
 			<div id="right">
-				<button id="feed" class="interaction">FEED</button>
-				<div class="countdown" id="feed_count">Ready!</div>
+			<form action="interaction.php?cat_id=<?=$cat_id?>" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="feed" value=1></input>
+				<button id="feed" class="interaction" <?=disableIfUnready($feed_time)?>>FEED</button>
+			</form>
+				<div class="countdown" id="feed_count"><?=$feed_time?></div>
 				<br>
-				<button id="play" class="interaction">PLAY</button>
-				<div class="countdown" id="play_count">Ready!</div>
+			<form action="interaction.php?cat_id=<?=$cat_id?>" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="play" value=1></input>
+				<button id="play" class="interaction" <?=disableIfUnready($play_time)?>>PLAY</button>
+			</form>	
+				<div class="countdown" id="play_count"><?=$play_time?></div>
 				<br>
-				<button id="pet" class="interaction">PET</button>
-				<div class="countdown" id="pet_count">Ready!</div>
+			<form action="interaction.php?cat_id=<?=$cat_id?>" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="pet" value=1></input>
+				<button id="pet" class="interaction" <?=disableIfUnready($pet_time)?>>PET</button>
+			</form>
+				<div class="countdown" id="pet_count"><?=$pet_time?></div>
 				<br>
-				<button id="groom" class="interaction">GROOM</button>
-				<div class="countdown" id="groom_count">Ready!</div>
+			<form action="interaction.php?cat_id=<?=$cat_id?>" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="groom" value=1></input>	
+				<button id="groom" class="interaction" <?=disableIfUnready($groom_time)?>>GROOM</button>
+			</form>
+				<div class="countdown" id="groom_count"><?=$groom_time?></div>
 			</div>
 		</content>
 		
 		<footer> 
-			<!-- 
-			this is a placeholder, update if we want something else here
-			-->
 			<p id="footer_info">CS 372 Fall 2020</p>
 		</footer>
 		
 	</body>
 	
 </html>
+
+<?PHP
+//FUNCTIONS
+function describeHair($hair_number) {
+	if($hair_number == 1) {
+		return "Short";
+	} elseif($hair_number == 2) { 
+		return "Medium";
+	} elseif($hair_number == 3) { 
+		return "Long";
+	}
+}
+
+function getReadyTime($interactionTimestamp, $cooldown_time) {
+	//Make sure you're on Saskatchewan time for this to work.
+	if (time() - strtotime($interactionTimestamp) > $cooldown_time) {
+		return "Ready!";
+	} else {
+		return  "Will be ready at: " . date("H:i d/m/Y ", strtotime($interactionTimestamp) + $cooldown_time);
+	}
+}
+
+function disableIfUnready($interactionTime) {
+	if ($interactionTime == "Ready!") {
+		return "";
+	} else {
+		return "disabled";
+	}
+}
+
+function updateMoney($new_money) {
+	//database variables
+	$dbserver = "34.121.103.176:3306";
+	$dbusername = "testuser1587";
+	$dbpassword = "woai1587";
+	$dbname = "catsdatabase";
+	
+	$database = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+	if ($database->connect_error) {
+		die("Connection failed: " . $database->connect_error);
+	}
+	$uid = $_SESSION["user"];
+	$query = "UPDATE user_table SET money = '$new_money' WHERE user_id = '$uid';";
+	$database->query($query);
+	$database->close();
+	
+	$_SESSION["money"] = $new_money;
+}
+
+function setTimestamp($timer_name, $cat_id) {
+	//database variables
+	$dbserver = "34.121.103.176:3306";
+	$dbusername = "testuser1587";
+	$dbpassword = "woai1587";
+	$dbname = "catsdatabase";
+	
+	$database = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+	if ($database->connect_error) {
+		die("Connection failed: " . $database->connect_error);
+	}
+	//Update this timer
+	$query = "UPDATE cat_table SET " . $timer_name . " = NOW() WHERE cat_id = '$cat_id';";
+	$database->query($query);
+	//Update the last interacted with timestamp
+	$query = "UPDATE cat_table SET interaction_timer = NOW() WHERE cat_id = '$cat_id';";
+	$database->query($query);
+	$database->close();
+}
+?>
